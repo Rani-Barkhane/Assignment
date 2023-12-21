faa5ccc66d244eb56430a108aa6b2cb9e6fa3ddc
Rani-Barkhane <rani.barkhane2018@gmail.com>
2023-12-21 10:44:52 +0530

    cluster with redis

--- app-cluster.js
+++ app-cluster.js
@@ -1,23 +1,24 @@
 const cluster = require('cluster');
 const os = require('os');
+const redis = require('redis');
 
-// Check if the current process is the master process
 if (cluster.isMaster) {
-  // Get the number of available CPUs
   const numCPUs = os.cpus().length;
 
-  // Fork workers equal to the number of available CPUs - 1
   for (let i = 0; i < numCPUs - 1; i++) {
     cluster.fork();
   }
 
-  // Handle the event when a worker exits
   cluster.on('exit', (worker, code, signal) => {
     console.log(`Worker ${worker.process.pid} exited with code ${code} and signal ${signal}`);
-    // Fork a new worker to replace the exited one
     cluster.fork();
   });
 } else {
-  // This code is executed in each worker process
+  const client = redis.createClient();
+
+  client.on('connect', () => {
+    console.log('Connected to Redis');
+  });
+
   require('./app');
 }
